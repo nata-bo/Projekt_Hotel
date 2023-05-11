1a23305c4b01cdcd014893186609d97eb33f76a5
Natalia Bodnaryuk <bodnata.o@gmail.com>
2023-05-11 14:34:05 +0200

    add new code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Room r1 = new Room(1,2,RoomCategory.STANDARD);
+        Room r2 = new Room(2,4,RoomCategory.LUX);
+        Room r3 = new Room(3,2,RoomCategory.SUITE);
+        Room r4 = new Room(4,3,RoomCategory.FAMILY);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+
+        Booking booking = new Booking(
+                new Person("Nick","Nicolson"),r1,
+                new MyDate(11,5,2023),
+                new MyDate(15,5,2023)
+        );
+
+        BookingProcessing bookingProcessing = new BookingProcessing();
+        bookingProcessing.add(booking);
+        System.out.println(bookingProcessing);
+
+        bookingProcessing.add(new Person("Ann","Virt"),r2,
+                new MyDate(11,5,2023),
+                new MyDate(15,5,2023));
+        System.out.println(bookingProcessing);
+
+
+
+
     }
 }
